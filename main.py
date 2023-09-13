8b7e844575577218dc99c645e4982169e6262a6b
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-09-13 11:35:31 -0400

    add ChimneySweeper class

--- main.py
+++ main.py
@@ -7,43 +7,44 @@ import uasyncio
 from servo import servo2040
 from servo import ServoCluster
 
-SERVO_PIN_START = servo2040.SERVO_1
-SERVO_PIN_END = servo2040.SERVO_1
-
-gc.collect()
+DEFAULT_SERVO_PIN_START = servo2040.SERVO_1
+DEFAULT_SERVO_PIN_END = servo2040.SERVO_1
 
 
 class ChimneySweepers:
-    """Chimney sweepers representation."""
-
     def __init__(
-        self,
-        start: int = SERVO_PIN_START,
-        stop: int = SERVO_PIN_END,
-    ) -> None:
+            self,
+            start: int = DEFAULT_SERVO_PIN_START,
+            end: int = DEFAULT_SERVO_PIN_END,
+        ) -> None:
         self.start = start
-        self.end = stop
-        pins = list(range(self.start, self.end + 1))
-        self.cluster = ServoCluster(0, 0, pins)
+        self.end = end
 
-    async def step(self) -> None:
-        await self.cluster.enable_all()
-        await uasyncio.sleep_ms(200)
-        await self.cluster.all_to_min()
-        await uasyncio.sleep_ms(2000)
-        await self.cluster.all_to_max()
-        await uasyncio.sleep_ms(2000)
-        await self.cluster.disable_all()
+    def create(self) -> ServoCluster:
+        gc.collect()
+        pins = list(range(self.start, self.end + 1))
+        cluster = ServoCluster(0, 0, pins)
+        return cluster
 
     async def run(self) -> None:
+        cluster = self.create()
         while True:
-            await self.step()
-            await uasyncio.sleep_ms(200)
+            cluster.enable_all()
+            await uasyncio.sleep_ms(2000)
+            cluster.all_to_min()
+            await uasyncio.sleep(2)
+            cluster.all_to_max()
+            await uasyncio.sleep(2)
+            cluster.disable_all()
+            await uasyncio.sleep_ms(2000)
 
 
-async def main() -> None:
+async def main():
     sweepers = ChimneySweepers()
     uasyncio.create_task(sweepers.run())
+    while True:
+        # read meter
+        await uasyncio.sleep_ms(200)
 
 
 if __name__ == "__main__":
