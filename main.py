8628e315d9df002aa55eab9566bc23d55c066386
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-09-13 22:39:18 -0400

    add meter class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import gc
 import uasyncio
 
+from plasma import WS2812
 from servo import servo2040
 from servo import ServoCluster
 
@@ -38,8 +39,24 @@ class ChimneySweepers:
             await uasyncio.sleep_ms(200)
 
 
+class ServoCurrentMeter:
+    """Servo current load meter."""
+
+    async def get_leds(self) -> WS2812:
+        num_leds = servo2040.NUM_LEDS
+        led_data = servo2040.LED_DATA
+        leds = WS2812(
+            num_leds,
+            pio=1,
+            sm=0,
+            led_data,
+        )
+        return leds
+
+
 async def main():
     sweepers = ChimneySweepers()
+    meter = ServoCurrentMeter()
     uasyncio.create_task(sweepers.run())
     while True:
         # read meter
