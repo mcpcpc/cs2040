f0ab45e7cc9b5b21b1a38b1b9156cd0d1f09b3f6
Michael Czigler <michaelczigler@icloud.com>
2023-09-14 09:00:03 -0400

    move create objects outside of classes

--- main.py
+++ main.py
@@ -26,18 +26,56 @@ def get_level(num_leds: int, i: int) -> float:
     return level
 
 
+def create_servo_cluster() -> ServoCluster:
+    """Create and return new ServoCluster object."""
+
+    gc.collect()
+    start = servo2040.SERVO_1
+    end = servo2040.SERVO_1
+    pins = list(range(start, end + 1))
+    cluster = ServoCluster(0, 0, pins)
+    return cluster
+
+
+def create_leds() -> WS2812:
+    """Create and return WS2812 array."""
+ 
+    leds = WS2812(
+        num_leds=servo2040.NUM_LEDS,
+        pio=1,
+        sm=0,
+        dat=servo2040.LED_DATA,
+    )
+    return leds
+
+
+def create_current_adc() -> Analog:
+    """Create and return current ADC."""
+
+    current_adc = Analog(
+        servo2040.SHARED_ADC,
+        servo2040.CURRENT_GAIN,
+        servo2040.SHUNT_RESISTOR,
+        servo2040.CURRENT_OFFSET,
+    )
+    return current_adc
+
+
+def create_analog_mux() -> AnalogMux:
+    """Create and return analog multiplexer."""
+
+    muxed_pin = machine.Pin(servo2040.SHARED_ADC)
+    analog_mux = AnalogMux(
+        servo2040.ADC_ADDR_0,
+        servo2040.ADC_ADDR_1,
+        servo2040.ADC_ADDR_2,
+        muxed_pin=muxed_pin,
+    )
+    return analog_mux
+
+
 class ChimneySweepers:
     """Chimney sweepers representation."""
-
-    async def create(self) -> ServoCluster:
-        """Create and return new ServoCluster object."""
-
-        gc.collect()
-        start = servo2040.SERVO_1
-        end = servo2040.SERVO_1
-        pins = list(range(start, end + 1))
-        cluster = ServoCluster(0, 0, pins)
-        return cluster
 
     async def step(self, cluster) -> None:
         """Step servo motors to new position."""
@@ -50,7 +88,7 @@ class ChimneySweepers:
     async def run(self) -> None:
         """Run servo motors in process loop."""
 
-        cluster = await self.create()
+        cluster = create_servo_cluster()
         while True:
             await self.step(cluster)
             await uasyncio.sleep_ms(200)
@@ -59,47 +97,36 @@ class ChimneySweepers:
 class ServoCurrentMeter:
     """Servo current meter representation."""
 
-    async def create_leds(self) -> WS2812:
-        """Create and return WS2812 array."""
- 
-        leds = WS2812(
-            num_leds=servo2040.NUM_LEDS,
-            pio=1,
-            sm=0,
-            dat=servo2040.LED_DATA,
-        )
-        return leds
+    async def get_measurement(self, adc: Analog) -> float:
+        """Get measurement from ADC object."""
 
-    async def create_current_adc(self) -> Analog:
-        """Create and return current ADC."""
- 
-        current_adc = Analog(
-            servo2040.SHARED_ADC,
-            servo2040.CURRENT_GAIN,
-            servo2040.SHUNT_RESISTOR,
-            servo2040.CURRENT_OFFSET,
-        )
-        return current_adc
+        current = 0.0
+        for i in range(5):
+            current += adc.read_current()
+            await uasyncio.sleep_ms(200)
+        current_average = current / 5
+        return current_average
 
-    async def create_analog_mux(self) -> AnalogMux:
-        """Create and return analog multiplexer."""
+    async def step(self) -> None:
+        """Step through current measurement process."""
 
-        muxed_pin = machine.Pin(servo2040.SHARED_ADC)
-        analog_mux = AnalogMux(
-            servo2040.ADC_ADDR_0,
-            servo2040.ADC_ADDR_1,
-            servo2040.ADC_ADDR_2,
-            muxed_pin=muxed_pin,
-        )
-        return analog_mux
+        
+
+    async def run(self) -> None:
+        """Run servo current meter in loop."""
+
+        leds = create_leds()
+        current_adc = create_current_adc()
+        analog_mux = create_analog_mux()
+        analog_mux.select(servo2040.CURRENT_SENSE_ADDR)
 
 
 async def main():
     sweepers = ChimneySweepers()
     meter = ServoCurrentMeter()
     uasyncio.create_task(sweepers.run())
+    #uasyncio.create_task(meter.run())
     while True:
-        # read meter
         await uasyncio.sleep_ms(200)
 
 
