15453df799422afe189a2eaa7d080bbd7bd92533
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-09-22 22:58:39 -0400

    add translation lib

---
+++ lib/translate.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+
+class TranslationBase:
+    """Translation base representation."""
+
+    def __init__(
+        self,
+        start: float,
+        end: float,
+        duration_ms: int
+     ) -> None:
+        self.start = start
+        self.end = end
+        self.duration_ms = duration_ms
+
+    def __call__(self, time_ms: int) -> float:
+        return self.ease(time_ms)
+
+    def function(self, t: float) -> int:
+        return NotImplementedError
+ 
+    def ease(self, time_ms: int) -> float:
+        a = self.function(time_ms / self.duration_ms)
+        return (self.end * a) + self.start * (1 - a)
+
+
+class Linear(TranslationBase):
+    """Linear translation representation."""
+
+    def function(self, t: float) -> int:
+        return t
+
+ 
+ class ServoClusterTranslate:
+    """Servo translate representation."""
+
+    start: float = -1.0
+    end: float = 1.0
+    duration: int = 1000
+    transition: TransitionBase = Linear(
+        start=self.start,
+        end=self.end,
+        duration=self.duration,
+    )
+    current_time: int = 0
+    start_time: int = 0
+
+    def __init__(self, servo) -> None:
+        self.servo = servo
+
+    def tick_start(self) -> None:
+        """Servo tick start."""
+
+        self.start_time = time.ticks_ms()
+        self.transition.start = self.start
+        self.transition.end = self.end
+        self.transition.duration = self.duration
+
+    def tick(self) -> bool:
+        """Servo tick."""
+
+        current_time = time.ticks_ms()
+        ellapsed = time.ticks_diff(
+            current_time,
+            self.start_time,
+        )
+        if ellapsed > self.duration:
+            self.current_position = self.end
+            self.servo.to_position(self.current_position)
+            return True
+        self.current_position = self.transition.ease(ellapsed)
+        self.servo.to_position(self.current_position)
+        if self.current_position == self.end:
+            return True
+        else:
+            return False
