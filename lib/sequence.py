7bc3ba821b14214428955f6ddeb75567221f5a96
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-09-24 11:59:42 -0400

    add sequence.py

---
+++ lib/sequence.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class SequenceBase:
+    """Sequence base representation."""
+
+    take: int = 8  # sequence length
+    minimum: float = -1.0  # mininum position value
+    maximum: float = 1.0  # maximum position value
+
+    def __call__(self):
+        result = self.generator(self.sequence())
+        return result
+
+    def sequence(self) -> bytearray:
+        """User implemented sequence."""
+
+        return NotImplementedError
+
+    @classmethod
+    def normalize(self, value: int) -> float:
+        """Normalize sequence value."""
+
+        return value / 0xFF
+
+    @classmethod
+    def position(self, value: float) -> float:
+        """Compute position from normalized value."""
+
+        delta = self.maximum - self.minimum
+        position = value * delta + self.minimum
+        return float(position)
+
+    @classmethod
+    def generator(self, value: bytearray):
+        """Positional array generator."""
+        length = len(value)
+        for i in range(0, length, self.take):
+            seq = value[i:i + self.take]
+            norm = list(map(self.normalize, seq))
+            yield list(map(self.position, norm))
+
+
+class AlternatingOctet(SequenceBase):
+    """Alternating Octet
+    
+    Moves eight servos in alternating full-min nd full-max
+    positions.
+    """
+
+    def sequence(self):
+        return [
+            0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF,
+            0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00
+        ]
