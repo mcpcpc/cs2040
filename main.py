9745997bdbf4cc8f3cfa56969fdc1af239429036
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-10-02 16:45:01 -0400

    multi-threading (#4)
    # Added
    
    * Multi-threading functionality
    * Servo easing and translation using inverse hyperbolic function

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ __status__ = "Production"
 
 import gc
 import machine
-import uasyncio
+import time
+import _thread
+
 
 from pimoroni import Analog
 from pimoroni import AnalogMux
@@ -19,165 +21,256 @@ from plasma import WS2812
 from servo import servo2040
 from servo import ServoCluster
 
-# servo parameters
-SERVO_CLUSTER_PIN_START = servo2040.SERVO_1
-SERVO_CLUSTER_PIN_END = servo2040.SERVO_7
-SERVO_CLUSTER_DELAY = 5000 # in milliseconds
-
-# meter parameters
-METER_LOAD_MAX_AMPERES = 3.0
-METER_LED_BRIGHTNESS_OFF = 0.1
-METER_LED_BRIGHTNESS_ON = 0.5
-METER_LED_NUMBER = servo2040.NUM_LEDS
-
-
-def get_hue(num_leds: int, index: int) -> float:
-    """Computer and return LED meter hue value."""
-
-    hue = (1.0 - index / (num_leds - 1)) * 0.333
-    return hue
-
-
-def get_level(num_leds: int, index: int) -> float:
-    """Computer and return LED meter level value."""
-
-    level = (index + 0.5) / num_leds
-    return level
-
-
-def get_load(current: float, max_current: float) -> float:
-    """Computer and return current load utilization."""
-
-    load = current / max_current
-    return load
-
 
 def create_servo_cluster() -> ServoCluster:
     """Create and return new ServoCluster object."""
 
     gc.collect()
-    start = SERVO_CLUSTER_PIN_START
-    end = SERVO_CLUSTER_PIN_END
+    start = servo2040.SERVO_1
+    end = servo2040.SERVO_7
     pins = list(range(start, end + 1))
-    cluster = ServoCluster(0, 0, pins)
-    return cluster
+    return ServoCluster(0, 0, pins)
 
 
 def create_leds() -> WS2812:
     """Create and return WS2812 array."""
  
-    leds = WS2812(
+    return WS2812(
         num_leds=servo2040.NUM_LEDS,
         pio=1,
         sm=0,
         dat=servo2040.LED_DATA,
     )
-    return leds
 
 
 def create_current_adc() -> Analog:
     """Create and return current ADC."""
 
-    current_adc = Analog(
+    return Analog(
         servo2040.SHARED_ADC,
         servo2040.CURRENT_GAIN,
         servo2040.SHUNT_RESISTOR,
         servo2040.CURRENT_OFFSET,
     )
-    return current_adc
 
 
 def create_analog_mux() -> AnalogMux:
     """Create and return analog multiplexer."""
 
     muxed_pin = machine.Pin(servo2040.SHARED_ADC)
-    analog_mux = AnalogMux(
+    return AnalogMux(
         servo2040.ADC_ADDR_0,
         servo2040.ADC_ADDR_1,
         servo2040.ADC_ADDR_2,
         muxed_pin=muxed_pin,
     )
-    return analog_mux
 
 
-class ChimneySweepers:
-    """Chimney sweepers representation."""
-
-    def __init__(self, cluster: ServoCluster) -> None:
-        self.cluster = cluster
-
-    async def step(self) -> None:
-        """Step servo sequence."""
-
-        count = self.cluster.count()
-        for servo in range(count):
-            if (servo % 2) == 0:
-                self.cluster.to_min(servo)
-            else:
-                self.cluster.to_max(servo)
-            await uasyncio.sleep_ms(500)
-        await uasyncio.sleep_ms(SERVO_CLUSTER_DELAY)
-        for servo in range(count):
-            if (servo % 2) == 0:
-                self.cluster.to_max(servo)
-            else:
-                self.cluster.to_min(servo)
-            await uasyncio.sleep_ms(500)
-        await uasyncio.sleep_ms(SERVO_CLUSTER_DELAY)
-
-    async def run(self) -> None:
-        """Run servo motors in process loop."""
-
-        while True:
-            await self.step()
-            await uasyncio.sleep_ms(200)
-
-
-class ServoCurrentMeter:
+class LoadCurrentMeter:
     """Servo current meter representation."""
 
-    def __init__(self, leds: WS2812, adc: Analog, mux: AnalogMux) -> None:
+    max_load = 3.0 # amperes
+    num_leds = servo2040.NUM_LEDS 
+    brightness_on = 0.4
+    brightness_off = 0.1
+
+    def __init__(
+        self,
+        leds: WS2812,
+        adc: Analog,
+        mux: AnalogMux,
+    ) -> None:
         self.leds = leds
         self.adc = adc
         self.mux = mux
 
-    async def step(self) -> None:
-        """Step through current measuremsent process."""
+    def get_hue(self, index: int) -> float:
+        """Computer and return LED meter hue value."""
 
-        current = self.adc.read_current()
-        percent = get_load(current, METER_LOAD_MAX_AMPERES)
-        for i in range(METER_LED_NUMBER):
-            hue = get_hue(METER_LED_NUMBER, i)
-            level = get_level(METER_LED_NUMBER, i)
-            if percent >= level:
-                self.leds.set_hsv(i, hue, 1.0, METER_LED_BRIGHTNESS_ON)
-            else:
-                self.leds.set_hsv(i, hue, 1.0, METER_LED_BRIGHTNESS_OFF) 
+        hue = (1.0 - index / (self.num_leds - 1)) * 0.333
+        return float(hue)
 
-    async def run(self) -> None:
-        """Run servo current meter in loop."""
+    def get_level(self, index: int) -> float:
+        """Computer and return LED meter level value."""
+
+        level = (index + 0.5) / self.num_leds
+        return float(level)
+
+    def get_load(self, value: float) -> float:
+        """Computer and return current load utilization."""
+
+        load = value / self.max_load
+        return float(load)
+
+    def initialize(self) -> None:
+        """Initialize current meter."""
 
         self.mux.select(servo2040.CURRENT_SENSE_ADDR)
         self.leds.start()
+
+    def step(self) -> None:
+        """Step through current measuremsent process."""
+
+        current = self.adc.read_current()
+        percent = self.get_load(current)
+        for i in range(self.num_leds):
+            hue = self.get_hue(i)
+            level = self.get_level(i)
+            if percent >= level:
+                self.leds.set_hsv(
+                    i,
+                    hue,
+                    1.0,
+                    self.brightness_on,
+                )
+            else:
+                self.leds.set_hsv(
+                    i,
+                    hue,
+                    1.0,
+                    self.brightness_off,
+                ) 
+
+    def run(self) -> None:
+        """Run servo current meter in loop."""
+
+        self.initialize()
         while True:
-            await self.step()
-            await uasyncio.sleep_ms(100)
+            self.step()
 
 
-async def main():
-    """Main asynchronous method for task scheduling."""
+class TranslateBase:
+    """Translation base representation."""
 
-    # Declare pin assignments and I/O objects
-    cluster = create_servo_cluster()
-    sweepers = ChimneySweepers(cluster)
+    start: float = -1.0
+    end: float = 1.0
+    duration_ms: int = 5000
+
+    def __call__(self, time_ms: int) -> float:
+        return self.ease(time_ms)
+
+    def function(self, t: float) -> int:
+        """User implemented translation function."""
+
+        return NotImplementedError
+
+    def ease(self, time_ms: int) -> float:
+        """Ease postion from current time in milliseconds."""
+
+        a = self.function(time_ms / self.duration_ms)
+        return a * (self.end - self.start) + self.start
+
+
+class Ease_out_quad(TranslateBase):
+
+    def function(self, t: float) -> float:
+        return -(t * (t - 2))
+
+
+class ServoTickBase:
+    """Servo tick base representation."""
+
+    start_ms: int = 0
+    min_position: float = -1.0
+    max_position: float = 1.0
+
+    def __init__(
+        self,
+        cluster: ServoCluster,
+        translate: TranslateBase
+    ) -> None:
+        self.cluster = cluster
+        self.translate = translate
+
+    def initialize(self) -> None:
+        """Initialization."""
+
+        self.start_ms = time.ticks_ms()
+
+    def to_position(self, servo: int, position: float) -> None:
+        """Trigger servo to position."""
+
+        self.cluster.to_percent(
+            servo,
+            position,
+            self.min_position,
+            self.max_position,
+        )
+
+    def tick(self, servo: int) -> bool:
+        """Single tick in motion."""
+
+        ellapsed_ms = time.ticks_ms() - self.start_ms
+        if ellapsed_ms > self.translate.duration_ms:
+            position = self.translate.end
+            self.to_position(servo, position)
+            return True
+        position = self.translate.ease(ellapsed_ms)
+        self.to_position(servo, position)
+        if position == self.translate.end:
+            return True
+        return False
+
+
+
+class ChimneySweepers(ServoTickBase):
+    """Chimney sweepers representation."""
+
+    sequence: list = [
+        [-1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0],
+        [1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0],
+    ]
+
+    def setup(self) -> None:
+        """Servo setup."""
+
+        count = self.cluster.count()
+        for servo in range(count):
+            self.cluster.to_min(servo)
+            time.sleep_ms(500)
+
+    def tick_all(self, seq: list, prev: list) -> list[bool]:
+        """Tick all."""
+        
+        result = []
+        servos = range(self.cluster.count())
+        for servo, start, end in zip(servos, prev, seq):
+            self.translate.start = start
+            self.translate.end = end
+            result.append(self.tick(servo))
+        return result
+
+    def step(self) -> None:
+        """Step servo position."""
+
+        for s, seq in enumerate(self.sequence):
+            prev = self.sequence[s - 1]
+            status = [False] * self.cluster.count()
+            self.initialize()
+            while not all(status):
+                status = self.tick_all(seq, prev)
+
+    def run(self) -> None:
+        """Run servo motors in process loop."""
+
+        while True:
+            self.step()
+
+
+def main():
+    """Main method for task scheduling."""
+
     adc = create_current_adc()
     leds = create_leds()
     mux = create_analog_mux()
-    meter = ServoCurrentMeter(leds, adc, mux)
-    # Initialize asynchronous tasks
-    uasyncio.create_task(meter.run())
-    await sweepers.run()
+    cluster = create_servo_cluster()
+    translate = Ease_out_quad()
+    sweepers = ChimneySweepers(cluster, translate)
+    meter = LoadCurrentMeter(leds, adc, mux)
+    _thread.start_new_thread(meter.run, ())
+    sweepers.setup()
+    sweepers.run()
 
 
 if __name__ == "__main__":
-    uasyncio.run(main())
+    main()
