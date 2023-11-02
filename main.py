d4d00cc45c0d872c74c88c5db424ffe27ccb2f48
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-11-01 21:21:43 -0400

    NeoPixel Driver (#7)
    ## Added
    
    * NeoPixel functionality

--- main.py
+++ main.py
@@ -3,31 +3,42 @@
 
 """main.py: CS-2040 Servo Controller Module"""
 
-__author__ = "Michael Czigler"
-__copyright__ = "Copyright 2023, Michael Czigler"
-__version__ = "1.0.0"
-__license__ = "BSD-3-Clause"
-__status__ = "Production"
+# MicroPython built-in libraries
+from collections import deque
+from gc import collect
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep_ms
+from time import ticks_diff
+from time import ticks_ms
+from _thread import start_new_thread
+from _thread import allocate_lock
+from _thread import LockType
 
-import collections
-import gc
-import machine
-import time
-import _thread
-
+# Pimoroni libraries
 from pimoroni import Analog
 from pimoroni import AnalogMux
 from plasma import WS2812
 from servo import servo2040
 from servo import ServoCluster
 
+__version__ = "1.0.0"
+
+RGBW_BLACK = const((0, 0, 0, 0))
+RGBW_RED = const((255, 0, 0, 0))
+RGBW_ORANGE = const((255, 40, 0, 0))
+RGBW_YELLOW = const((255, 150, 0, 0))
+RGBW_GREEN = const((0, 255, 0, 0))
+RGBW_BLUE = const((0, 0, 255, 0))
+RGBW_VIOLET = const((180, 0, 255, 0))
+
 
 def create_servo_cluster() -> ServoCluster:
     """Create and return new ServoCluster object."""
 
-    gc.collect()
+    collect()
     start = servo2040.SERVO_1
-    end = servo2040.SERVO_7
+    end = servo2040.SERVO_8
     pins = list(range(start, end + 1))
     return ServoCluster(0, 0, pins)
 
@@ -57,13 +68,20 @@ def create_current_adc() -> Analog:
 def create_analog_mux() -> AnalogMux:
     """Create and return analog multiplexer."""
 
-    muxed_pin = machine.Pin(servo2040.SHARED_ADC)
+    muxed_pin = Pin(servo2040.SHARED_ADC)
     return AnalogMux(
         servo2040.ADC_ADDR_0,
         servo2040.ADC_ADDR_1,
         servo2040.ADC_ADDR_2,
         muxed_pin=muxed_pin,
     )
+
+
+def create_rgbw_neopixels() -> NeoPixel:
+    """Create and return neopixels."""
+
+    din_pin = Pin(servo2040.SDA) 
+    return NeoPixel(din_pin, 8, bpp=4)
 
 
 class LoadCurrentMeter:
@@ -112,7 +130,7 @@ class LoadCurrentMeter:
             self.leds.set_hsv(i, h, 1.0, v)
         return True 
 
-    def run(self, lock: _thread.Lock = None) -> None:
+    def run(self, lock: LockType) -> None:
         """Run servo current meter in loop."""
 
         lock.acquire()
@@ -157,7 +175,7 @@ class SequenceBase:
 
     def __init__(self, items: list) -> None:
         maxlen = len(items)
-        self.deque = collections.deque((), maxlen)
+        self.deque = deque((), maxlen)
         for item in items:
             self.deque.append(item)
 
@@ -177,10 +195,12 @@ class ChimneySweepers:
     def __init__(
         self,
         cluster: ServoCluster,
+        neopixels: NeoPixel,
         sequences: SequenceBase,
         translate: TranslateBase,
     ) -> None:
         self.cluster = cluster
+        self.neopixels = neopixels
         self.sequences = sequences
         self.translate = translate
 
@@ -192,8 +212,8 @@ class ChimneySweepers:
     def tick(self, servo: int) -> bool:
         """Single tick in motion."""
 
-        ticks_ms = time.ticks_ms()
-        ellapsed_ms = time.ticks_diff(ticks_ms, self.start_ms)
+        now_ms = ticks_ms()
+        ellapsed_ms = ticks_diff(now_ms, self.start_ms)
         if ellapsed_ms > self.translate.duration_ms:
             position = self.translate.end
             self.to_position(servo, position)
@@ -208,26 +228,27 @@ class ChimneySweepers:
         """Servo step."""
 
         status = [False]
-        self.start_ms = time.ticks_ms()
+        self.start_ms = ticks_ms()
         while not all(status):
             status = []
-            for servo, start, end, duration in sequences:
+            for servo, start, end, duration, color in sequences:
                 self.translate.start = start
                 self.translate.end = end
                 self.translate.duration_ms = duration
                 status.append(self.tick(servo))
+                self.neopixels[servo] = color
+        self.neopixels.write()
 
-    def setup(self) -> None:
-        """Servo setup."""
+    def run(self, lock: LockType) -> None:
+        """Run servo motors in process loop."""
 
         count = self.cluster.count()
         for servo in range(count):
+            sleep_ms(500)
+            self.neopixels[servo] = RGBW_BLACK
             self.cluster.to_min(servo)
-            time.sleep_ms(500)
-
-    def run(self, lock: _thread.LockType) -> None:
-        """Run servo motors in process loop."""
-
+        self.neopixels.write()
+        sleep_ms(3000)
         while not lock.acquire(0):
             sequences = self.sequences()
             self.step(sequences)
@@ -239,36 +260,63 @@ def main():
     adc = create_current_adc()
     leds = create_leds()
     mux = create_analog_mux()
+    neopixels = create_rgbw_neopixels()
     cluster = create_servo_cluster()
     sequences = SequenceBase(
         items=[
             [
-                (0, -1.0, 1.0, 5000),
-                (1, 1.0, -1.0, 5000),
-                (2, -1.0, 1.0, 5000),
-                (3, 1.0, -1.0, 5000),
-                (4, -1.0, 1.0, 5000),
-                (5, 1.0, -1.0, 5000),
-                (6, -1.0, 1.0, 5000),
+                (0, -1.0, 1.0, 5000, RGBW_RED),
+                (1, 1.0, -1.0, 5000, RGBW_BLACK),
+                (2, -1.0, 1.0, 5000, RGBW_YELLOW),
+                (3, 1.0, -1.0, 5000, RGBW_BLACK),
+                (4, -1.0, 1.0, 5000, RGBW_BLUE),
+                (5, 1.0, -1.0, 5000, RGBW_BLACK),
+                (6, -1.0, 1.0, 5000, RGBW_VIOLET),
+                (7, -1.0, 0.5, 5000, RGBW_BLACK),
             ],
             [
-                (0, 1.0, -1.0, 5000),
-                (1, -1.0, 1.0, 5000),
-                (2, 1.0, -1.0, 5000),
-                (3, -1.0, 1.0, 5000),
-                (4, 1.0, -1.0, 5000),
-                (5, -1.0, 1.0, 5000),
-                (6, 1.0, -1.0, 5000),
+                (0, 1.0, -1.0, 5000, RGBW_BLACK),
+                (1, -1.0, 1.0, 5000, RGBW_ORANGE),
+                (2, 1.0, -1.0, 5000, RGBW_BLACK),
+                (3, -1.0, 1.0, 5000, RGBW_GREEN),
+                (4, 1.0, -1.0, 5000, RGBW_BLACK),
+                (5, -1.0, 1.0, 5000, RGBW_VIOLET),
+                (6, 1.0, -1.0, 5000, RGBW_BLACK),
+                (7, 0.5, -0.5, 5000, RGBW_BLACK),
+            ],
+            [
+                (0, -1.0, 1.0, 5000, RGBW_VIOLET),
+                (1, 1.0, -1.0, 5000, RGBW_BLACK),
+                (2, -1.0, 1.0, 5000, RGBW_ORANGE),
+                (3, 1.0, -1.0, 5000, RGBW_BLACK),
+                (4, -1.0, 1.0, 5000, RGBW_GREEN),
+                (5, 1.0, -1.0, 5000, RGBW_BLACK),
+                (6, -1.0, 1.0, 5000, RGBW_VIOLET),
+                (7, -0.5, 0.0, 5000, RGBW_BLACK),
+            ],
+            [
+                (0, 1.0, -1.0, 5000, RGBW_BLACK),
+                (1, -1.0, 1.0, 5000, RGBW_RED),
+                (2, 1.0, -1.0, 5000, RGBW_BLACK),
+                (3, -1.0, 1.0, 5000, RGBW_YELLOW),
+                (4, 1.0, -1.0, 5000, RGBW_BLACK),
+                (5, -1.0, 1.0, 5000, RGBW_BLUE),
+                (6, 1.0, -1.0, 5000, RGBW_BLACK),
+                (7, 0.0, -1.0, 5000, RGBW_BLACK),
             ],
         ]
     )
     translate = Ease_in_quad()
-    sweepers = ChimneySweepers(cluster, sequences, translate)
+    sweepers = ChimneySweepers(
+        cluster,
+        neopixels,
+        sequences,
+        translate, 
+    )
     meter = LoadCurrentMeter(leds, adc, mux)
-    lock = _thread.allocate_lock()
-    _thread.start_new_thread(meter.run, (lock,))
-    time.sleep_ms(200)  # allow time for meter lock
-    sweepers.setup()
+    lock = allocate_lock()
+    start_new_thread(meter.run, (lock,))
+    sleep_ms(200)  # allow time for meter lock
     sweepers.run(lock)
 
 
